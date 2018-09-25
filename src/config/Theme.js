5059deb127af817a625695ec0ddc2cf7a0e69073
Claudio Junior <claudio.1091@gmail.com>
2018-09-25 20:03:55 -0300

    Criação do tema da App

---
+++ src/config/Theme.js
@@ -0,0 +1,32 @@
+export default {
+  font: {
+    body: {
+      fontSize: '16px',
+      fontFamily: "'Montserrat', sans-serif",
+      fontWeight: 'normal'
+    },
+    title: {
+      fontSize: '1.25em'
+    },
+    subtitle: {
+      fontSize: '.87em',
+      fontWeight: 'bold'
+    },
+    headline: {
+      fontSize: '1.5em'
+    },
+    buttonLabel: {
+      fontSize: '.87em',
+      fontWeight: 'bold'
+    }
+  },
+  colors: {
+    background: '#ffffff',
+    white: '#ffffff',
+    boxShadow: 'rgba(158,158,158,.6)',
+    primary: '#005b95',
+    black_87: '#212121',
+    black_54: '#757575',
+    black_38: '#9e9e9e'
+  }
+};
