286b692904e4b53598242488e9ef91041b137762
Claudio Junior <claudio.1091@gmail.com>
2018-09-30 18:23:41 -0300

    Refatoração do Theme.js: Adição do ThemeProvider do StyledComponents

--- src/config/Theme.js
+++ src/config/Theme.js
@@ -1,32 +1,13 @@
-export default {
-  font: {
-    body: {
-      fontSize: '16px',
-      fontFamily: "'Montserrat', sans-serif",
-      fontWeight: 'normal'
-    },
-    title: {
-      fontSize: '1.25em'
-    },
-    subtitle: {
-      fontSize: '.87em',
-      fontWeight: 'bold'
-    },
-    headline: {
-      fontSize: '1.5em'
-    },
-    buttonLabel: {
-      fontSize: '.87em',
-      fontWeight: 'bold'
-    }
-  },
-  colors: {
-    background: '#ffffff',
-    white: '#ffffff',
-    boxShadow: 'rgba(158,158,158,.6)',
-    primary: '#005b95',
-    black_87: '#212121',
-    black_54: '#757575',
-    black_38: '#9e9e9e'
-  }
+export const baseTheme = {
+  primaryColor: '#005b95',
+  blackPrimaryColor: '#212121',
+  blackSecondaryColor: '#757575',
+  blackTertiaryColor: '#9e9e9e',
+  shadowColor: 'rgba(158,158,158,.4)',
+  whiteColor: '#ffffff',
+
+  headlineFontSize: '1.5em',
+  titleFontSize: '1.25em',
+  subtitleFontSize: '0.87em',
+  buttonFontSize: '0.87em',
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import styled, { ThemeProvider } from 'styled-components';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import { baseTheme } from './config/Theme';
+
+const App = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
+  <ThemeProvider theme={baseTheme}>
+    <App>
+      <BrowserRouter>
+        <Switch />
+      </BrowserRouter>
+    </App>
+  </ThemeProvider>,
+  document.getElementById('root'),
 );
