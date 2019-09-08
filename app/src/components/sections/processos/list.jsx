3463013ff8e422592bcfbbc714ec9e6940c95787
Brynner Ferreira <ferreira@brynner.net>
2019-09-08 17:25:51 -0300

    Actions in the list of items

--- app/src/components/sections/processos/list.jsx
+++ app/src/components/sections/processos/list.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import FirebaseService from '../../../services/FirebaseService';
 
+import {Link} from "react-router-dom";
+import {routes} from "../../../utils/RouterUtil";
+
+import { Container, Row, Col, Button } from 'react-bootstrap';
+
 export default class List extends Component {
 
   state = {
@@ -11,12 +16,18 @@ export default class List extends Component {
     FirebaseService.getDataList('processos', (dataReceived) => this.setState({data: dataReceived}))
   }
 
+  componentWillUnmount() {
+    FirebaseService.disconnect('processos');
+  }
+
   remove = (id) => {
     FirebaseService.remove(id, 'processos');
   };
-  
-  look = () => {
-    console.log(this.state.data);
+
+  edit = (id) => {
+    console.log(id);
+
+    this.props.history.push(`${routes.edit.path}+7`); // routes.edit.path
   }
 
   items = (data) => {
@@ -26,12 +37,18 @@ export default class List extends Component {
     data.map((item, index) => {
       return(
         items.push(
-          <div key={index}>
-            <label>{item.subject}</label>
-            <p>{item.description}</p>
-            <b onClick={() => this.remove(item.key)}>Remove {item.key}</b>
-            <hr />
-          </div>
+          <Row key={index}>
+            <Col xs={12} sm={6}>
+              <label>{item.subject}</label>
+              <p>{item.description}</p>
+              <Button variant="danger" onClick={() => this.remove(item.key)}>Remove</Button>
+              
+              <Link to={routes.edit.pathWithouParam + item.key}>
+                <Button variant="primary">Editar</Button>
+              </Link>
+
+            </Col>
+          </Row>
         )
       )
     });
@@ -41,10 +58,13 @@ export default class List extends Component {
 
   render() {
     return (
-      <main onClick={() => this.look()}>
-        <h3>List</h3>
-
-        {this.items(this.state.data)}
+      <main>
+        <Container>
+          <Link to={routes.add.path}>{routes.add.name}</Link>
+          <hr />
+          <h3>List</h3>
+          {this.items(this.state.data)}
+        </Container>
       </main>
     )
   }
