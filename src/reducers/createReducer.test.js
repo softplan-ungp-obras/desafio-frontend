00295e1eea36ecaf4ac6a92bac887a11661874c0
marcioc0sta <marciocst2@gmail.com>
2020-03-09 21:02:06 -0300

    add tests for create reducer

---
+++ src/reducers/createReducer.test.js
@@ -0,0 +1,29 @@
+import createReducer from './createReducer';
+
+describe('createReducer test', () => {
+  it('should return the initial state', () => {
+    // given
+    const defaultState = {};
+    const mockaction = () => {};
+    // when
+    const reducer = createReducer(defaultState, {
+      MOCK_ACTION: mockaction
+    });
+    // then
+    expect(reducer(defaultState, 'MOCK_ACTION')).toEqual(defaultState);
+  });
+
+  it('should return the result current handler', () => {
+    // given
+    const InitialState = {
+      name: ''
+    };
+    // when
+    const reducer = createReducer(InitialState, {
+      MOCK_ACTION: (InitialState.name = 'john')
+    });
+
+    // then
+    expect(reducer(InitialState, 'MOCK_ACTION')).toEqual({ name: 'john' });
+  });
+});
